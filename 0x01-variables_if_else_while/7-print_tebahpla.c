6f6704761c33a02849185dcce7ea58a05c1fe2af
Franco <vandenberg.fw@gmail.com>
2022-06-03 14:35:45 -0700

    7. Smile in the mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,9 +6,12 @@
  */
 int main(void)
 {
-char alphabet;
-for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
-putchar(alphabet);
-putchar('\n');
-return (0);
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
 }
