fcb12c6997a161165db4503dc1f187e921e0e9e4
Franco <vandenberg.fw@gmail.com>
2022-06-03 05:34:29 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * print single digit numbers base 10 from 0
+ * Return: 0
+ */
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+printf("%d", number);
+printf("\n");
+return (0);
+}
