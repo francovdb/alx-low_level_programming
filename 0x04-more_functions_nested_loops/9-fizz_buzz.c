057a04c66db3554c617c4d8f7b9fbf92e27b1c14
Franco <vandenberg.fw@gmail.com>
2022-06-09 14:25:15 -0700

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * prints message
+ * Return: 0
+ */
+
+int main(void)
+{
+	int Cntr;
+
+	for (Cntr = 1; Cntr <= 100; Cntr++)
+	{
+		if ((Cntr % 3 == 0) && (Cntr % 5 == 0))
+			printf("FizzBuzz");
+
+		else if (Cntr % 5 == 0)
+			printf("Buzz");
+
+		else if (Cntr % 3 == 0)
+			printf("Fizz");
+
+		else
+			printf("%i", Cntr);
+
+		if (Cntr < 100)
+			printf(" ");
+	}
+	return (0);
+}
