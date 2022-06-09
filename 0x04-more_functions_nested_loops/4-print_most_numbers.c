fabdc999415e13c5cec1758c87805980e626889f
Franco <vandenberg.fw@gmail.com>
2022-06-09 12:47:17 -0700

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,7 @@ void print_most_numbers(void)
 
 	for (num = '0'; num <= 9; num++)
 	{
-		if (num == '2' || num == '4')
-		{
-			
-		}
-		else
+		if (num != '2' && num != '4')
 		{
 			_putchar(num);
 		}
