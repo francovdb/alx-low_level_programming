ab5972563af167777b15f8e15daf2de6d4f042df
Franco <vandenberg.fw@gmail.com>
2022-06-13 13:22:15 -0700

    6. Half the lies they tell about me arent true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry Point
+ * prints every other character of a string, starting with the first character
+ * @str: string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int Cntr;
+
+	for ( Cntr = 0; str[Cntr] != '\0'; Cntr++)
+	{
+		if (Cntr % 2 == 0)
+		{
+			_putchar(str[Cntr]);
+		}
+	}
+
+	_putchar('\n');
+}
