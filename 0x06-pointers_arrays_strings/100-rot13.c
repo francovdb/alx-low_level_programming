813b1d462f0dd583e44a24620c6dc593d3edc9a4
Franco <vandenberg.fw@gmail.com>
2022-06-16 13:36:04 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * rot13 - Entry Point
+ * encodes a string using rot13
+ * @p: pointer to string
+ * Return: p
+ */
+
+char *rot13(char *p)
+{
+	int Cntr1;
+	int Cntr2;
+	char alphabet[52] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+			     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+	     		     'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+	     		     'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+	     		     'Z'};
+	char rot13 = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+     		     'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+     		     'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+     		     'M'};
+
+	for (Cntr1 = 0; p[Cntr1] != '\0'; Cntr++)
+	{
+		for (Cntr2 = 0; Cntr < 53; Cntr2++)
+		{
+			if (alphabet[Cntr2] == p[Cntr1])
+			{
+				p[Cntr1] = rot13[Cntr2];
+				break;
+			}
+		}
+	}
+	return (p);
+}
