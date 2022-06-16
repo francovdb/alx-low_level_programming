e0e976a8483495fd9ee73077836d6faa0f43b265
Franco <vandenberg.fw@gmail.com>
2022-06-16 11:11:44 -0700

    2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.c"
+
+/**
+ * _strncpy - Entry Point
+ * copies the string
+ * @dest: destination
+ * @src: source
+ * @n: number of bytes
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int Cntr;
+
+	for (Cntr = 0; Cntr < n && src[Cntr] != '\0'; Cntr++)
+	{
+		dest[Cntr] = src[Cntr];
+	}
+	for (; Cntr < n; Cntr++)
+	{
+		dest[Cntr] = '\0';
+	}
+	return (dest);
+}
