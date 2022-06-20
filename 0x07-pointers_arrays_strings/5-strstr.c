47ffc0fd9614041144924a92f794e2b010933a94
Franco <vandenberg.fw@gmail.com>
2022-06-20 12:04:26 -0700

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,28 +10,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int Cntr;
-
-	if (*needle == 0)
-		return (haystack);
-
-	while (*haystack)
-	{
-		Cntr = 0;
-
-		if (haystack[Cntr] == needle[Cntr])
-		{
-			do{
-				if (needle[Cntr + 1] == '\0')
-					return (haystack);
-
-				Cntr++;
-
-			} while (haystack[Cntr] == needle[Cntr]);
-		}
-
-		haystack++;
-	}
-
-	return ('\0');
+int Cntr;
+if (*needle == 0)
+return (haystack);
+while (*haystack)
+{
+Cntr = 0;
+if (haystack[Cntr] == needle[Cntr])
+{
+do{
+if (needle[Cntr + 1] == '\0')
+return (haystack);
+Cntr++;
+} while (haystack[Cntr] == needle[Cntr]);
+}
+haystack++;
+}
+return ('\0');
 }
