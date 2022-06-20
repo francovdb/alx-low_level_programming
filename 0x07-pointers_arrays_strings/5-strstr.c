6fe8fd6ec6851691345fdd0c8fb33de9e1d7c660
Franco <vandenberg.fw@gmail.com>
2022-06-20 10:30:41 -0700

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry Point
+ * locate a substring
+ * @haystack: string to search
+ * @needle: substring to locate
+ * Return: NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int Cntr;
+
+	if (*needle == 0)
+	{
+		return (haystack);
+	}
+
+	while (*haystack)
+	{
+		Cntr = 0;
+
+		if (haystack[Cntr] == needle[Cntr])
+		{
+			do{
+				if (needle[Cntr + 1] == '\0')
+				{
+					return (haystack);
+				}
+
+				Cntr++;
+
+			} while (haystack[Cntr] == needle[Cntr]);
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
