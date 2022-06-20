ee46626f94364f4ff2c1bc990bf3c0a88631772a
Franco <vandenberg.fw@gmail.com>
2022-06-20 09:30:46 -0700

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,10 +3,10 @@
 /**
  * _memset - Entry Point
  * fills memory with a constant byte
- * @n: memory byte
  * @s: pointer
- * @b: constant byte
- * Return: pointer s
+ * @b: byte value
+ * @n: memory
+ * Return: dest
  */
 
 char *_memset(char *s, char b, unsigned int n)
