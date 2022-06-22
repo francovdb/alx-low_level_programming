59cab52d2f01e90a0cc2c67f150de0162af22e93
Franco <vandenberg.fw@gmail.com>
2022-06-22 08:42:16 -0700

    6. Inception. Is it possible?

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _prime_number - Entry Point
+ * returns 1 if the input integer is a prime number, otherwise return 0
+ * @n: number
+ * Return: 1 or 0
+ */
+
+int actual_prime(int n, int Cntr);
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - Entry Point
+ * returns 1 if the input integer is a prime number, otherwise return 0
+ * @n: number
+ * @Cntr: Counter
+ * Return: 1 or 0
+ */
+
+int actual_prime(int n, int Cntr)
+{
+	if (Cntr == 1)
+	{
+		return (1);
+	}
+	if (n % Cntr == && Cntr > 0)
+	{
+		return (0);
+	}
+
+	return (actual_prime(n, i - 1));
+}
