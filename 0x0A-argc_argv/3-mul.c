9ca62192ed7fe0802f744390faf706b818adbb3c
Franco <vandenberg.fw@gmail.com>
2022-06-24 13:36:20 -0700

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * multiplies two numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int number1;
+	int number2;
+	int product;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	number1 = atoi(argv[1]);
+	number2 = atoi(argv[2]);
+	product = number1 * number2;
+
+	printf("%d\n", product);
+
+	return (0);
+}
