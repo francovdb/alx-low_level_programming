65063051f7cea26297860641aeebae056858f269
Franco <vandenberg.fw@gmail.com>
2022-06-24 13:24:52 -0700

    0. It aint what they call you, its what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry Point
@@ -8,9 +9,8 @@
  * Return: 0
  */
 
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc __attribute__((unused)), char **argv)
 {
-	printf("%s\n", argv[0]);
-	
+	printf("%s\n", *argv);
 	return (0);
 }
