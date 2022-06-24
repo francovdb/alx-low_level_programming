ac0ed833e5c80a088c4bbfa96be457ceadde423b
Franco van den Berg <vandenberg.fw@gmail.com>
2022-06-24 23:29:02 +0200

    Upload C File

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - Entry Point
+ * computes absolute value of integer
+ * @n: computed integer
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
