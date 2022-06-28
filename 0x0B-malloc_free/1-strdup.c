1534601ec1ef0586f807809308908029127f16f5
Franco <vandenberg.fw@gmail.com>
2022-06-28 12:09:15 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 
 /**
- * _strdup = Entry Point
- * return pointer to new allocated space in memory with a copy of the string
- * @str: string
- * Return: NULL
+ * _strdup - Returns a pointer to a newly-allocated space in memory
+ * containing a copy of the string given as parameter.
+ * @str: The string to be copied.
+ * Return: If str == NULL or insufficient memory is available - NULL.
+ * Otherwise - a pointer to the duplicated string.
  */
 
 char *_strdup(char *str)
