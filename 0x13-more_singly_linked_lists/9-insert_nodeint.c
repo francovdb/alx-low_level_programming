ef27ba093d4b2e8e84fc140b5684385463ba66c0
Franco <vandenberg.fw@gmail.com>
2022-07-19 13:47:47 -0700

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,6 +17,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	headcopy = *head;
 	new = malloc(sizeof(listint_t));
 
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+
 	if (idx == 0)
 	{
 		new->next = headcopy;
