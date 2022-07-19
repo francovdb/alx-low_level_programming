d98c0d3edd95d11d54fb4a46f646a5dc99372989
Franco <vandenberg.fw@gmail.com>
2022-07-19 13:05:36 -0700

    update header

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,9 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef LISTS_H_
+#define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct listint_s - Entry Point
@@ -15,6 +17,19 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/**
+ * listp_s - Entry Point
+ * singly linked list of pointers
+ * @p: pointer
+ * @next: next node
+ */
+
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
