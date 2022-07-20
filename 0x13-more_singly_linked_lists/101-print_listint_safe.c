bc3227ba7bf3f33ea6d1ad08c5d8de92dfd74ca6
Franco <vandenberg.fw@gmail.com>
2022-07-20 00:11:22 -0700

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
 
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+
 /**
  * looped_listint_len - Entry Point
  * Counts the number of unique nodes
